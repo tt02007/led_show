04a4226e99da72e6b9eb042e4cf8fbaed23551f5
tt02007 <91233572+tt02007@users.noreply.github.com>
2021-09-30 10:57:38 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
-for (let x = 0; x <= 4; x++) {
-    led.plot(x, 0)
-    basic.pause(500)
+function LED_show () {
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            led.plot(x, y)
+            basic.pause(500)
+        }
+    }
+    basic.clearScreen()
+}
+let x = 0
+let y = 0
+for (let index = 0; index < 3; index++) {
+    LED_show()
 }
